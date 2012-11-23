ecade0d0544d174d930f2db49b46d19754afc7e2
kirill klimov <ra22341@ya.ru>
2012-11-23 20:01:11 +0400

    add dynamic tagging and funny tag numeric

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -25,6 +25,7 @@ require("awful/widget/calendar2")
 require("revelation")
 
 --require("shifty")
+require("eminent")
 
 -- useful for debugging, marks the beginning of rc.lua exec
 print("Entered rc.lua: " .. os.time())
@@ -147,12 +148,34 @@ layouts =
 -- }}}
 
 -- {{{ Tags
--- Define a tag table which hold all screen tags.
+-- Define a tag table which hold all screen tags
+-- taglist numerals
+--- arabic, chinese, {east|persian}_arabic, roman, thai, random
+taglist_numbers = "roman" -- we support arabic (1,2,3...),
+
+st_numbers_langs = { 'arabic', 'chinese', 'east_arabic', 'persian_arabic', }
+taglist_numbers_sets = {
+    arabic={ 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+    chinese={"一", "二", "三", "四", "五", "六", "七", "八", "九", "十"},
+    east_arabic={'١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'}, -- '٠' 0
+    persian_arabic={'٠', '١', '٢', '٣', '۴', '۵', '۶', '٧', '٨', '٩'},
+    roman={'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'},
+    thai={'๑', '๒', '๓', '๔', '๕', '๖', '๗', '๘', '๙', '๑๐'},
+}
+
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     --tags[s] = awful.tag({ "w", "B", "T", 4, 5, 6, 7, 8, 9 }, s, layouts[1])
-    tags[s] = awful.tag({ "α", "β", "γ", "δ", "ε", "η", "θ", "λ", "ω" }, s, layouts[1])
+    --tags[s] = awful.tag({ "α", "β", "γ", "δ", "ε", "η", "θ", "λ", "ω" }, s, layouts[1])
+    --tags[s] = awful.tag({"一", "二", "三", "四", "五", "六", "七", "八", "九", "十"}, s, layouts[1])
+    if taglist_numbers == 'random' then
+        math.randomseed(os.time())
+        local taglist = taglist_numbers_sets[taglist_numbers_langs[math.random(table.getn(taglist_numbers_langs))]]
+        tags[s] = awful.tag(taglist, s, layouts[1])
+    else
+        tags[s] = awful.tag(taglist_numbers_sets[taglist_numbers], s, layouts[1])
+    end
 end
 
 --Tags with default layout
